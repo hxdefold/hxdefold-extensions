e6cbdaf6e561111548d1a5667a925fe2be495708
Haath <haath@protonmail.com>
2022-10-19 12:35:05 +0200

    Fix webview placeholders

--- src/defold/extensions/webview/WebView.hx
+++ src/defold/extensions/webview/WebView.hx
@@ -9,59 +9,64 @@ extern class WebView
 {
     /**
      * Creates a webview instance. It can show HTML pages as well as evaluate Javascript. The view remains hidden until the first call. There can exist a maximum of 4 webviews at the same time.
-     * On iOS, the callback will never get a `webview.CALLBACK_RESULT_EVAL_ERROR`, due to the iOS SDK implementation." 
-     * 
-     * @param callback A callback which receives info about finished requests taking the following parameters: 
+     * On iOS, the callback will never get a `webview.CALLBACK_RESULT_EVAL_ERROR`, due to the iOS SDK implementation."
+     *
+     * @param callback A callback which receives info about finished requests taking the following parameters:
      */
-    static function create(callback: UNKNOWN): Void;
+    static function create<T>(callback: (self: T, webview_id: WebviewId, request_id: RequestId, type: Callback, data: AnyTable)->Void): WebviewId;
 
     /**
-     * Destroys an instance of a webview. 
-     * 
-     * @param webview_id The webview id (returned by the `webview.create()` call) 
+     * Destroys an instance of a webview.
+     *
+     * @param webview_id The webview id (returned by the `webview.create()` call)
      */
-    static function destroy(webview_id: UNKNOWN): Void;
+    static function destroy(webview_id: WebviewId): Void;
 
     /**
-     * Opens a web page in the webview, using HTML data. Once the request is done, the callback (registered in `webview.create()`) is invoked. 
-     * 
+     * Opens a web page in the webview, using HTML data. Once the request is done, the callback (registered in `webview.create()`) is invoked.
+     *
      * @param webview_id The webview id
      * @param html The HTML data to display
-     * @param options A table of options for the request. See `webview.open()` 
+     * @param options A table of options for the request. See `webview.open()`
      */
-    static function open_raw(webview_id: UNKNOWN, html: UNKNOWN, options: UNKNOWN): Void;
+    static function open_raw(webview_id: WebviewId, html: String, options: AnyTable): Void;
 
     /**
-     * Evaluates JavaScript within the context of the currently loaded page (if any). Once the request is done, the callback (registered in `webview.create()`) is invoked. The callback will get the result in the `data["result"]` field. 
-     * 
+     * Evaluates JavaScript within the context of the currently loaded page (if any). Once the request is done, the callback (registered in `webview.create()`) is invoked. The callback will get the result in the `data["result"]` field.
+     *
      * @param webview_id The webview id
-     * @param code The JavaScript code to evaluate 
+     * @param code The JavaScript code to evaluate
      */
-    static function eval(webview_id: UNKNOWN, code: UNKNOWN): Void;
+    static function eval(webview_id: WebviewId, code: String): Void;
 
     /**
-     * Shows or hides a webview 
-     * 
+     * Shows or hides a webview
+     *
      * @param webview_id The webview id
-     * @param visible If `0`, hides the webview. If non zero, shows the view 
+     * @param visible If `0`, hides the webview. If non zero, shows the view
      */
-    static function set_visible(webview_id: UNKNOWN, visible: UNKNOWN): Void;
+    static function set_visible(webview_id: WebviewId, visible: Int): Void;
 
     /**
-     * Returns the visibility state of the webview. 
-     * 
-     * @param webview_id The webview id 
+     * Returns the visibility state of the webview.
+     *
+     * @param webview_id The webview id
      */
-    static function is_visible(webview_id: UNKNOWN): Void;
+    static function is_visible(webview_id: WebviewId): Bool;
 
     /**
-     * Sets the position and size of the webview 
-     * 
+     * Sets the position and size of the webview
+     *
      * @param webview_id The webview id
      * @param x The x position of the webview
      * @param y The y position of the webview
      * @param width The width of the webview (-1 to match screen width)
-     * @param height The height of the webview (-1 to match screen height) 
+     * @param height The height of the webview (-1 to match screen height)
      */
-    static function set_position(webview_id: UNKNOWN, x: UNKNOWN, y: UNKNOWN, width: UNKNOWN, height: UNKNOWN): Void;
+    static function set_position(webview_id: WebviewId, x: Int, y: Int, width: Int, height: Int): Void;
 }
+
+
+extern class WebviewId { }
+
+extern class RequestId { }
