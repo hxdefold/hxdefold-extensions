c554ed06b32bfa0a0318441c9dc668ef1c25d823
Haath <haath@protonmail.com>
2022-10-27 15:31:41 +0200

    Fix adinfo externs

--- src/defold/extensions/adinfo/AdInfo.hx
+++ src/defold/extensions/adinfo/AdInfo.hx
@@ -4,11 +4,16 @@ import lua.Table.AnyTable;
 import defold.types.*;
 
 
+/**
+ * Retrieve an advertising identifier for a device or user. Advertisers use this to id so they can deliver customized advertising.
+ * The id is used for tracking and identifying a user (without revealing personal information).
+ * On iOS this extension also supports the App Tracking Transparency framework.
+ */
 @:native("adinfo")
 extern class AdInfo
 {
     /**
-     * Get a table with advertising information. [icon:attention] function returns nil if values do not ready  
+     * Get a table with advertising information. [icon:attention] function returns nil if values do not ready
      */
-    static function get(): Void;
+    static function get<T>(callback: (self: T, info: Info)->Void): Void;
 }
---
+++ src/defold/extensions/adinfo/Info.hx
@@ -0,0 +1,9 @@
+package defold.extensions.adinfo;
+
+
+extern class Info
+{
+    var ad_ident: Int;
+
+    var ad_tracking_enabled: Bool;
+}
